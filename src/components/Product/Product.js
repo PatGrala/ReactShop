6ba03aef7793dadff4890b57f68a5f84f9ead5d2
Patryk Grala <patrykgrala@gmail.com>
2023-03-13 15:25:48 +0100

    Add useMemo

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,6 +1,6 @@
 import styles from './Product.module.scss';
 import PropTypes from 'prop-types';
-import {useState } from 'react';
+import {useMemo, useState } from 'react';
 import ProductImage from '../ProductImage/ProductImage';
 import ProductForm from '../ProductForm/ProductForm'
 
@@ -15,12 +15,12 @@ const Product = props => {
     console.log('Color' + currentColor)
   }
 
-  const getPrice = () => {
-    return (props.basePrice + props.sizes.find((size) => currentSize === size.name).additionalPrice);
-  };
-
   const [currentColor, setCurrentColor] = useState(props.colors[0]);
   const [currentSize, setCurrentSize] = useState(props.sizes[0].name);
+
+  const getPrice = useMemo(() => {
+    return (props.basePrice + props.sizes.find((size) => currentSize === size.name).additionalPrice);
+  }, [props.basePrice, props.sizes, currentSize]);
 
   
 
@@ -30,7 +30,7 @@ const Product = props => {
       <div>
         <header>
           <h2 className={styles.name}>{props.title}</h2>
-          <span className={styles.price}>Price: {getPrice()} $</span>
+          <span className={styles.price}>Price: {getPrice} $</span>
         </header>
         <ProductForm
           handleSubmit={handleSubmit}
